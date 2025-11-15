c7907483a6d371e9d8f7e05b1fa0b3d0c835842a
Fabrício Nascimento <fabriciocostadn@gmail.com>
2025-11-15 13:08:27 -0300

    feat: add route table for public subnets

---
+++ public_route_table.tf
@@ -0,0 +1,26 @@
+resource "aws_route_table" "eks_public_route_table" {
+  vpc_id = aws_vpc.eks-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks-igw.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "eks-public-route-table"
+    }
+
+  )
+}
+
+resource "aws_route_table_association" "association_eks_subnet_public_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+resource "aws_route_table_association" "association_eks_subnet_public_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
