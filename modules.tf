074d9220ba04af2206dfacd6c789708efd0617ef
Fabrício Nascimento <fabriciocostadn@gmail.com>
2025-12-26 19:34:25 -0300

    feat: add modules node-group and aws-controller

--- modules.tf
+++ modules.tf
@@ -11,3 +11,18 @@ module "eks_cluster" {
   public_subnet_1a = module.eks-network.eks_subnet_public_1a
   public_subnet_1b = module.eks-network.eks_subnet_public_1b
 }
+
+module "eks_managed_node_group" {
+  source                = "./modules/managed-node-group"
+  cluster_name          = module.eks_cluster.cluster_name
+  eks_subnet_private_1a = module.eks-network.eks_subnet_private_1a
+  eks_subnet_private_1b = module.eks-network.eks_subnet_private_1b
+  tags                  = local.tags
+
+}
+
+module "aws-load-balancer-controller" {
+  source = "./modules/aws-load-balancer-controller"
+  tags   = local.tags
+
+}
