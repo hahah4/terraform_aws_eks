9f72dd21ea7c6482838a8142b4464068091464c3
Fabrício Nascimento <fabriciocostadn@gmail.com>
2026-01-03 11:00:29 -0300

    feat: output ca of cluster

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -1,3 +1,14 @@
 output "cluster_name" {
   value = aws_eks_cluster.eks-cluster.id
 }
+
+output "oidc" {
+  value = aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.eks-cluster.endpoint
+}
+output "certificate_authority" {
+  value = aws_eks_cluster.eks-cluster.certificate_authority[0].data
+}
